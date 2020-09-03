9c98aa42d547f0d39c95e9f4d00b26b1665aad93
Star (Shun) Li <starlee.li@fatritech.com>
2020-09-03 16:33:14 +0800

    ui 修改 ui.js

--- packages/ui/lib/ui.js
+++ packages/ui/lib/ui.js
@@ -2,7 +2,16 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(ctx => {
-  ctx.body = 'Hello World';
+    ctx.request.header = {
+        'content-type': 'text/html'
+    }
+  ctx.body = `
+    <body style="background: rgba(0,0,0,.8)">
+        <div>
+            Hello World
+        </div>
+    </body>
+  `;
 });
 
 app.listen(3000);
