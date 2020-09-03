67251d62e6e93273a57b542485fb99424990ae70
Star (Shun) Li <starlee.li@fatritech.com>
2020-09-03 11:32:23 +0800

    测试 verdaccioio v1

--- packages/cli/bin/fatrife.js
+++ packages/cli/bin/fatrife.js
@@ -6,6 +6,7 @@ const program = require('commander')
 const download = require('download-git-repo')
 const fs = require('fs')
 const chalk = require('chalk')
+const inquirer = require('inquirer')
 
 function exsitFile(file){
     return fs.existsSync(file)
@@ -30,11 +31,52 @@ program
         if(exsitFile(name)) {
             return log(chalk.red("当前目录下已存在该项目名称的目录或文件"))
         }
-       
-        // 下载远端的代码
+
+        // inquirer
+        //     .prompt([
+        //         {
+        //             type: 'confirm',
+        //             name: 'toBeDelivered',
+        //             message: 'Is this for delivery?',
+        //             default: false
+        //         },
+        //         {
+        //             type: 'input',
+        //             name: 'phone',
+        //             message: "What's your phone number?",
+        //             validate: function(value) {
+        //             var pass = value.match(
+        //                 /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+        //             );
+        //             if (pass) {
+        //                 return true;
+        //             }
+
+        //             return 'Please enter a valid phone number';
+        //             }
+        //         },
+        //         {
+        //             type: 'list',
+        //             name: 'size',
+        //             message: 'What size do you need?',
+        //             choices: ['Large', 'Medium', 'Small'],
+        //             filter: function(val) {
+        //               return val.toLowerCase();
+        //             }
+        //         },
+        //     ])
+        //     .then(answers => {
+        //         console.log('answers', answers)
+        //     })
+        //     .catch(error => {
+        //         console.log('answers', error)
+        //     })
+
+
+        //下载远端的代码
         download('FatriFE/fatri-vue-framework', name, err => {
             if(err) {
-                log(chalk.red('项目初始化失败'))  
+                log(chalk.red('项目初始化失败'))
             }else {
                 log(chalk.green('项目初始化成功'))
                 log(chalk.blue('安装依赖:'))
@@ -44,6 +86,16 @@ program
             }
         })
     })
+
+
+program
+  .command('dev')
+  .option('-m --mode <mode>', 'set dev mode')
+  .description('open server base on local')
+  .action((name, cmd) => {
+    console.log('dev', name, cmd)
+  })
+
 
 
 
